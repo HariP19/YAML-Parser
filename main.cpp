ed6d1687cd883fbdaacbe1baff546894e724d4cd
hari191197 <hari.191197@gmail.com>
2020-12-30 18:31:23 +0800

    stackoverflow edit

--- main.cpp
+++ main.cpp
@@ -6,27 +6,21 @@
 
 int main()
 {
-bool fileLoaded = false;
-YAML::Node config;
+    bool fileLoaded = false;
+    YAML::Node config;
 
-std::vector<float> target_jpos;
+    std::vector<float> target_jpos;
 
-try
-{
-    config = YAML::LoadFile("/config/test.yaml");
+    config = YAML::LoadFile("./config/test.yaml");
     fileLoaded = true;
-}
-catch(std::exception& e)
-{
-    fileLoaded = false;
-}
 
-target_jpos = config["target_jpos"].as<std::vector<float> >();
 
-if(fileLoaded)
-    std::cout << "File loaded successfully \n\t target_jpos[0]: " << target_jpos[0] << "\n";
-else
-    std::cout << "File failed to load\n";    
+    target_jpos = config["target_jpos"].as<std::vector<float> >();
+
+    if(fileLoaded)
+        std::cout << "File loaded successfully \n\t target_jpos[0]: " << target_jpos[0] << "\n";
+    else
+        std::cout << "File failed to load\n";    
     
     return 0;
 }
